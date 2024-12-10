36f56fd71fecbbd8b9b64b8b22e14629606fd8a1
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-10 20:18:12 +0800

    feat: :sparkles: add migration scripts for users table creation

---
+++ backend/migrations/000002_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    full_name TEXT NOT NULL,
+    role_id BIGINT NOT NULL REFERENCES roles(id)
+);

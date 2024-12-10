7907fb5cfe9a2e6c18cfb3d601bc4a1f6daf583d
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-10 20:37:15 +0800

    refactor: :recycle: restructure application package and integrate server initialization

--- backend/internal/application/application.go
+++ backend/internal/application/application.go
@@ -1,12 +1,13 @@
 package application
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/jonathanhu237/ecnc-shift-manager/backend/internal/config"
-	"github.com/jonathanhu237/ecnc-shift-manager/backend/internal/server"
 )
 
 type Application struct {
@@ -22,7 +23,14 @@ func New() *Application {
 	return &Application{
 		config: cfg,
 		logger: logger,
-		server: server.New(cfg, logger),
+		server: &http.Server{
+			Addr:         fmt.Sprintf(":%d", cfg.Server.Port),
+			Handler:      routes(),
+			IdleTimeout:  time.Minute,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		},
 	}
 }
 

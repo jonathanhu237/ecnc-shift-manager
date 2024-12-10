70a15d0f6a619f9932a5822848a38b7f8a5eaeec
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-10 23:04:39 +0800

    feat: :sparkles: add JSON read and write helper methods for HTTP requests

--- backend/internal/application/helpers.go
+++ backend/internal/application/helpers.go
@@ -3,6 +3,8 @@ package application
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
+	"net/http"
 	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -27,4 +29,23 @@ func (app *Application) openDB() (*sql.DB, error) {
 	}
 
 	return db, err
+}
+
+func (app *Application) readJSON(r *http.Request, dst any) error {
+	if err := json.NewDecoder(r.Body).Decode(dst); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *Application) writeJSON(w http.ResponseWriter, status int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		return err
+	}
+
+	return nil
 }

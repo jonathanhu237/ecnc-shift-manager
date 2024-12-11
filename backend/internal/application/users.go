5f05a99bb324013f2c951df37435d99544869aaa
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-11 11:12:12 +0800

    feat: :sparkles: implement user creation handler

---
+++ backend/internal/application/users.go
@@ -0,0 +1,75 @@
+package application
+
+import (
+	"net/http"
+
+	"github.com/jonathanhu237/ecnc-shift-manager/backend/internal/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func (app *Application) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	var payload struct {
+		Username string `json:"username" validate:"required"`
+		Password string `json:"password" validate:"required"`
+		Email    string `json:"email" validate:"required,email"`
+		FullName string `json:"full_name" validate:"required"`
+		Role     string `json:"role" validate:"required,oneof=普通助理 资深助理 黑心"`
+	}
+
+	if err := app.readJSON(r, &payload); err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	if err := app.validate.Struct(payload); err != nil {
+		app.validateError(w, r, err)
+		return
+	}
+
+	// check if username and email already exists
+	userExists, err := app.models.Users.CheckUserExists(payload.Username)
+	if err != nil {
+		app.internalSeverError(w, r, err)
+		return
+	}
+	if userExists {
+		app.errorResponse(w, r, http.StatusConflict, "username already exists")
+		return
+	}
+
+	emailExists, err := app.models.Users.CheckUserExists(payload.Email)
+	if err != nil {
+		app.internalSeverError(w, r, err)
+		return
+	}
+	if emailExists {
+		app.errorResponse(w, r, http.StatusConflict, "email already exists")
+		return
+	}
+
+	// hash the password
+	passwordHashBytes, err := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
+	if err != nil {
+		app.internalSeverError(w, r, err)
+		return
+	}
+
+	// create the user instance
+	user := &models.User{
+		Username:     payload.Username,
+		PasswordHash: string(passwordHashBytes),
+		Email:        payload.Email,
+		FullName:     payload.FullName,
+		Role:         payload.Role,
+	}
+
+	// insert the user instance to the database
+	if err := app.models.Users.InsertUser(user); err != nil {
+		app.internalSeverError(w, r, err)
+		return
+	}
+
+	// return a success message
+	if err := app.writeJSON(w, http.StatusCreated, user); err != nil {
+		app.internalSeverError(w, r, err)
+	}
+}
--- backend/internal/models/users.go
+++ backend/internal/models/users.go
@@ -1,6 +1,10 @@
 package models
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+)
 
 type User struct {
 	ID           int64  `json:"id"`
@@ -8,9 +12,55 @@ type User struct {
 	PasswordHash string `json:"-"`
 	Email        string `json:"email"`
 	FullName     string `json:"full_name"`
-	RoleID       int64  `json:"role_id"`
+	Role         string `json:"role"`
 }
 
 type UserModel struct {
 	DB *sql.DB
+}
+
+func (m *UserModel) CheckUserExists(username string) (bool, error) {
+	var exists bool
+	query := `SELECT EXISTS (SELECT 1 FROM users WHERE username = $1)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := m.DB.QueryRowContext(ctx, query, username).Scan(&exists); err != nil {
+		return false, err
+	}
+
+	return exists, nil
+}
+
+func (m *UserModel) CheckEmailExists(email string) (bool, error) {
+	var exists bool
+	query := `SELECT EXISTS (SELECT 1 FROM users WHERE email = $1)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := m.DB.QueryRowContext(ctx, query, email).Scan(&exists); err != nil {
+		return false, err
+	}
+
+	return exists, nil
+}
+
+func (m *UserModel) InsertUser(user *User) error {
+	query := `
+		INSERT INTO users (username, email, password_hash, full_name, role_id)
+		VALUES ($1, $2, $3, $4, (SELECT id FROM roles WHERE name = $5))
+		RETURNING id
+	`
+	args := []any{user.Username, user.Email, user.PasswordHash, user.FullName, user.Role}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.ID); err != nil {
+		return err
+	}
+
+	return nil
 }

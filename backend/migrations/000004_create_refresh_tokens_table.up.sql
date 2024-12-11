4345b4898b874f8506257493e68bd2476caf1fd0
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-11 15:18:58 +0800

    feat: :sparkles: add migration for refresh_tokens table

---
+++ backend/migrations/000004_create_refresh_tokens_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS refresh_tokens (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    refresh_token_hash TEXT NOT NULL,
+    issued_at TIMESTAMP DEFAULT NOW(),
+    expires_at TIMESTAMP NOT NULL,
+    revoked BOOLEAN DEFAULT FALSE
+);

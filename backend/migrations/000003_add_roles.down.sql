324d8e45910425520587d8b37682a3c993f8b189
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-11 11:02:00 +0800

    feat: :sparkles: update role migration scripts to include IDs and clear users table

--- backend/migrations/000003_add_roles.down.sql
+++ backend/migrations/000003_add_roles.down.sql
@@ -1,1 +1,3 @@
+DELETE FROM users;
+
 DELETE FROM roles WHERE name = '普通助理' OR name = '资深助理' OR name = '黑心';
--- backend/migrations/000003_add_roles.up.sql
+++ backend/migrations/000003_add_roles.up.sql
@@ -1,4 +1,4 @@
-INSERT INTO roles (name, level) VALUES 
-    ('普通助理', 1),
-    ('资深助理', 2),
-    ('黑心', 3);
+INSERT INTO roles (id, name, level) VALUES 
+    (1, '普通助理', 1),
+    (2, '资深助理', 2),
+    (3, '黑心', 3);

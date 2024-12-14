94e43ac0bcf8872ce29e39149c14d9b541bbc205
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-14 20:41:16 +0800

    refactor: :recycle: move the self-check to the application.go

--- backend/internal/application/application.go
+++ backend/internal/application/application.go
@@ -11,6 +11,7 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/jonathanhu237/ecnc-shift-manager/backend/internal/models"
 	"github.com/wneessen/go-mail"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Application struct {
@@ -102,4 +103,43 @@ func (app *Application) Run() {
 	if err := app.server.ListenAndServe(); err != nil {
 		app.logger.Error(err.Error())
 	}
+}
+
+func (app *Application) selfCheck() error {
+	if err := app.checkBlackcoreExists(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *Application) checkBlackcoreExists() error {
+	exists, err := app.models.Users.CheckBlackcoreExists()
+	if err != nil {
+		return err
+	}
+
+	random_password := app.generateRandomPassword(12)
+	password_hash, err := bcrypt.GenerateFromPassword([]byte(random_password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		user := &models.User{
+			Username:     "blackcore",
+			PasswordHash: string(password_hash), // ecnc_blackcore
+			Email:        "initialBlackcore@ecnc.com",
+			FullName:     "初始黑心",
+			Role:         "黑心",
+		}
+
+		if err := app.models.Users.InsertUser(user); err != nil {
+			return err
+		}
+
+		app.logger.Warn("blackcore does not exist, create a new one", slog.String("password", random_password))
+	}
+
+	return nil
 }

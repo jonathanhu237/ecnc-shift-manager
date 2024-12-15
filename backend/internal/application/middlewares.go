2df96462d23ef0ed53517517c6f81dd0dacf6313
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-15 21:58:13 +0800

    feat: :sparkles: add CORS middleware to handle cross-origin requests

--- backend/internal/application/middlewares.go
+++ backend/internal/application/middlewares.go
@@ -122,5 +122,23 @@ func (app *Application) getUserMiddleware(next http.Handler) http.Handler {
 		ctx := context.WithValue(r.Context(), userCtxKey, user)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
+}
 
+func (app *Application) corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if app.config.Environment == "development" {
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		}
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }

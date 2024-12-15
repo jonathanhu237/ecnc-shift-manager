de50d99601c76683360ca3b118eeca0ce4c8d5dd
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-15 22:28:41 +0800

    feat: :sparkles: configure dotenv for environment variables in Vite setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +12,8 @@ export default defineConfig({
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
+    },
+    define: {
+        "process.env": process.env,
     },
 });

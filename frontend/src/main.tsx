5febe6129548249975585e99d1f8f451a1756ea7
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-15 20:01:49 +0800

    feat: :sparkles: add auth layout and login page with routing support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,19 +2,26 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import AuthLayout from "./layouts/AuthLayout.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <App />
-                <ReactQueryDevtools initialIsOpen={false} />
-            </QueryClientProvider>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="auth" element={<AuthLayout />}>
+                        <Route path="login" element={<LoginPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     </StrictMode>
 );

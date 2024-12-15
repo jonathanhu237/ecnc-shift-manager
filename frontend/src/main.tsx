283df95e30d27fe34fe637848e3859d6d87f98f9
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-15 22:28:34 +0800

    feat: :sparkles: add UI components including Button, Card, Alert, Input, and Toaster

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import AuthLayout from "./layouts/AuthLayout.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
+import { Toaster } from "@/components/ui/sonner";
 
 const queryClient = new QueryClient();
 
@@ -22,6 +23,7 @@ createRoot(document.getElementById("root")!).render(
                 </Routes>
             </BrowserRouter>
             <ReactQueryDevtools initialIsOpen={false} />
+            <Toaster />
         </QueryClientProvider>
     </StrictMode>
 );

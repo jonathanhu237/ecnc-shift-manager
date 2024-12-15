ead81c02559a09c1d614531ff421f455909a3448
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-15 23:11:40 +0800

    feat: :sparkles: update routing structure to include Dashboard layout and index page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import AuthLayout from "./layouts/AuthLayout.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import { Toaster } from "@/components/ui/sonner";
+import DashboardLayout from "./layouts/DashboardLayout.tsx";
+import DashboardIndex from "./pages/DashboardIndex.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            retry: false,
+        },
+    },
+});
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<App />} />
+                    <Route path="/" element={<DashboardLayout />}>
+                        <Route index element={<DashboardIndex />} />
+                    </Route>
                     <Route path="auth" element={<AuthLayout />}>
                         <Route path="login" element={<LoginPage />} />
                     </Route>

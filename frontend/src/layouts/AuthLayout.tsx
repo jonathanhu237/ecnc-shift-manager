20b0ab8e284256a8d85876ddc7844458d936316b
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-15 22:29:11 +0800

    feat: :sparkles: implement login page

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,5 +1,9 @@
 import { Outlet } from "react-router";
 
 export default function AuthLayout() {
-    return <Outlet />;
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <Outlet />
+        </div>
+    );
 }
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,3 +1,152 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { useMutation } from "@tanstack/react-query";
+import { api, APIResponse } from "@/lib/api";
+import { AxiosResponse } from "axios";
+import { useState } from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle, Loader2 } from "lucide-react";
+import { toast } from "sonner";
+import { useNavigate } from "react-router";
+
+const formSchema = z.object({
+    username: z.string().min(1, {
+        message: "用户名不能为空",
+    }),
+    password: z.string().min(1, {
+        message: "密码不能为空",
+    }),
+});
+
 export default function LoginPage() {
-    return <div>LoginPage</div>;
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    });
+
+    const navigate = useNavigate();
+
+    const mutation = useMutation({
+        mutationFn: (data: z.infer<typeof formSchema>) => {
+            return api.post("/auth/login", data);
+        },
+        onSuccess: (res: AxiosResponse<APIResponse>) => {
+            const {
+                data: { code },
+            } = res;
+
+            if (code === 0) {
+                toast("登录成功");
+                navigate("/");
+            } else if (code === 1001) {
+                setLoginError("用户名或密码错误，请重新输入。");
+            }
+        },
+    });
+
+    const [loginError, setLoginError] = useState<string>("");
+
+    const onSubmit = (formData: z.infer<typeof formSchema>) => {
+        setLoginError("");
+        mutation.mutate(formData);
+    };
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>登录</CardTitle>
+                <CardDescription>
+                    请输入你的用户名 (NetID) 和密码以登录系统。
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-4"
+                    >
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>用户名</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="请输入你的用户名。"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>密码</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="password"
+                                            placeholder="请输入你的密码。"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription />
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        {loginError && (
+                            <Alert variant="destructive">
+                                <AlertCircle className="h-4 w-4" />
+                                <AlertTitle>登录失败</AlertTitle>
+                                <AlertDescription>
+                                    {loginError}
+                                </AlertDescription>
+                            </Alert>
+                        )}
+                        <Button
+                            type="submit"
+                            className="w-full"
+                            disabled={mutation.isPending}
+                        >
+                            {mutation.isPending ? (
+                                <>
+                                    <Loader2 className="animate-spin" />
+                                    请稍等
+                                </>
+                            ) : (
+                                "登录"
+                            )}
+                        </Button>
+                    </form>
+                </Form>
+            </CardContent>
+        </Card>
+    );
 }

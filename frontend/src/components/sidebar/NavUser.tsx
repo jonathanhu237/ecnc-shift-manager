ca583c719725677447a6d8fe5ed05ad2c18a69a0
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-18 23:01:23 +0800

    feat: :sparkles: add password update functionality in NavUser component

--- frontend/src/components/sidebar/NavUser.tsx
+++ frontend/src/components/sidebar/NavUser.tsx
@@ -1,8 +1,11 @@
+import { api, APIResponse } from "@/lib/api";
 import { UserType } from "@/types/user";
 import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { LogOut, Settings, User } from "lucide-react";
-import { Link, useNavigate } from "react-router";
+import { AxiosResponse } from "axios";
+import { KeyRound, LogOut, User } from "lucide-react";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
 import {
     DropdownMenuContent,
     DropdownMenuGroup,
@@ -11,9 +14,8 @@ import {
     DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
-import { api, APIResponse } from "@/lib/api";
-import { AxiosResponse } from "axios";
-import { toast } from "sonner";
+import { useState } from "react";
+import UpdatePasswordDialog from "../UpdatePasswordDialog";
 
 export default function NavUser() {
     const queryClient = useQueryClient();
@@ -30,48 +32,57 @@ export default function NavUser() {
             toast(err.message);
         },
     });
+    const [dialogOpen, setDialogOpen] = useState(false);
 
     return (
-        <SidebarMenu>
-            <SidebarMenuItem>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <SidebarMenuButton
-                            size="lg"
-                            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-                        >
-                            <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                                <User className="size-4" />
-                            </div>
-                            <div className="grid flex-1 text-left text-sm leading-tight">
-                                <span className="truncate font-semibold">
-                                    {myInfo?.full_name}
-                                </span>
-                                <span className="truncate text-xs">
-                                    {myInfo?.username}({myInfo?.role})
-                                </span>
-                            </div>
-                        </SidebarMenuButton>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent side="right" align="end">
-                        <DropdownMenuGroup>
-                            <Link to="/settings">
-                                <DropdownMenuItem>
-                                    <Settings />
-                                    设置
+        <>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <SidebarMenuButton
+                                size="lg"
+                                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                            >
+                                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                                    <User className="size-4" />
+                                </div>
+                                <div className="grid flex-1 text-left text-sm leading-tight">
+                                    <span className="truncate font-semibold">
+                                        {myInfo?.full_name}
+                                    </span>
+                                    <span className="truncate text-xs">
+                                        {myInfo?.username}({myInfo?.role})
+                                    </span>
+                                </div>
+                            </SidebarMenuButton>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent side="right" align="end">
+                            <DropdownMenuGroup>
+                                <DropdownMenuItem
+                                    onClick={() => setDialogOpen(true)}
+                                >
+                                    <KeyRound />
+                                    修改密码
                                 </DropdownMenuItem>
-                            </Link>
-                        </DropdownMenuGroup>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuGroup>
-                            <DropdownMenuItem onClick={() => mutation.mutate()}>
-                                <LogOut />
-                                登出
-                            </DropdownMenuItem>
-                        </DropdownMenuGroup>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </SidebarMenuItem>
-        </SidebarMenu>
+                            </DropdownMenuGroup>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuGroup>
+                                <DropdownMenuItem
+                                    onClick={() => mutation.mutate()}
+                                >
+                                    <LogOut />
+                                    登出
+                                </DropdownMenuItem>
+                            </DropdownMenuGroup>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </SidebarMenuItem>
+            </SidebarMenu>
+            <UpdatePasswordDialog
+                open={dialogOpen}
+                onOpenChange={setDialogOpen}
+            />
+        </>
     );
 }

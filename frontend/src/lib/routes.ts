3608e402fb7fa9d7d8ca5adab8b7d81754e3b670
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-18 23:13:03 +0800

    feat: :sparkles: add user level filtering for sidebar routes

--- frontend/src/lib/routes.ts
+++ frontend/src/lib/routes.ts
@@ -5,10 +5,12 @@ export const routes = [
         title: "主页",
         url: "/",
         icon: Home,
+        levelRequired: 1,
     },
     {
         title: "用户管理",
         url: "/users-management",
         icon: UserCog,
+        levelRequired: 3,
     },
 ];

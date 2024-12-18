f7d10bfd8fa036c8b7a902aed58a0a3a348e3f33
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-18 23:01:09 +0800

    refactor: :recycle: remove unused Settings route from routes.ts

--- frontend/src/lib/routes.ts
+++ frontend/src/lib/routes.ts
@@ -1,14 +1,9 @@
-import { Home, Settings } from "lucide-react";
+import { Home } from "lucide-react";
 
 export const routes = [
     {
         title: "主页",
         url: "/",
         icon: Home,
-    },
-    {
-        title: "设置",
-        url: "/settings",
-        icon: Settings,
     },
 ];

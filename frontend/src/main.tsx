53a47ba7b7c0b1bfc6420a1521f3f3e193f52ac9
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-18 23:01:01 +0800

    refactor: :recycle: reorganize imports in main.tsx for better readability

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { Toaster } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./index.css";
 import AuthLayout from "./layouts/AuthLayout.tsx";
-import LoginPage from "./pages/LoginPage.tsx";
-import { Toaster } from "@/components/ui/sonner";
 import DashboardLayout from "./layouts/DashboardLayout.tsx";
 import DashboardIndex from "./pages/DashboardIndex.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
 
 const queryClient = new QueryClient({
     defaultOptions: {

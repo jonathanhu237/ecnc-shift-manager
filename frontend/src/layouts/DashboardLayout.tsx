6340733be4fe30790ec51d94f688dc5ea38ba89c
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-18 16:31:13 +0800

    feat: :sparkles: add breadcrumb to the dashboard

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -1,20 +1,56 @@
 import AuthGuard from "@/components/auth/AuthGuard";
 import AppSidebar from "@/components/sidebar/AppSidebar";
 import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
     SidebarInset,
     SidebarProvider,
     SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { Outlet } from "react-router";
+import { routes } from "@/lib/routes";
+import { Link, Outlet, useLocation } from "react-router";
 
 export default function DashboardLayout() {
+    const location = useLocation();
+
     return (
         <AuthGuard>
             <SidebarProvider>
                 <AppSidebar />
                 <SidebarInset>
+                    <header className="flex h-12 items-center gap-2">
+                        <SidebarTrigger className="ml-2" />
+                        <Separator orientation="vertical" className="h-4" />
+                        <Breadcrumb>
+                            <BreadcrumbList>
+                                <BreadcrumbItem>
+                                    <BreadcrumbLink asChild>
+                                        <Link to="/">ECNC 假勤系统</Link>
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator />
+                                <BreadcrumbItem>
+                                    <BreadcrumbPage>
+                                        {
+                                            routes.find(
+                                                (item) =>
+                                                    item.url ==
+                                                    location.pathname
+                                            )?.title
+                                        }
+                                    </BreadcrumbPage>
+                                </BreadcrumbItem>
+                            </BreadcrumbList>
+                        </Breadcrumb>
+                    </header>
                     <main>
-                        <SidebarTrigger />
                         <Outlet />
                     </main>
                 </SidebarInset>

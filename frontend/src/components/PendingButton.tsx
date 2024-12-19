c5e584bac8985144feda6abf7cab3d2c9c50ce93
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-19 20:26:05 +0800

    feat: :sparkles: add PendingButton component with loading state

---
+++ frontend/src/components/PendingButton.tsx
@@ -0,0 +1,11 @@
+import { Loader2 } from "lucide-react";
+import { Button } from "./ui/button";
+
+export default function PendingButton() {
+    return (
+        <Button disabled>
+            <Loader2 className="animate-spin" />
+            请稍等
+        </Button>
+    );
+}

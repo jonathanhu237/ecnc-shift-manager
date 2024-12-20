6eba1cfc91e65cbcd0bdfb3ae233425f5d1ec748
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-20 17:50:53 +0800

    feat: :sparkles: add migration scripts for schedule templates

---
+++ backend/migrations/000005_create_schedule_tamplates_table.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS schedule_templates (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS schedule_template_shifts (
+    id BIGSERIAL PRIMARY KEY,
+    schedule_template_id BIGINT REFERENCES schedule_templates(id) ON DELETE CASCADE,
+    day_of_week INTEGER NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    assistants_required INTEGER NOT NULL
+);

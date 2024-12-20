ea683701097877e117fb0eee1abfd1454977ea28
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-20 18:10:42 +0800

    fix: :bug: update API response type

--- frontend/src/components/auth/AuthGuard.tsx
+++ frontend/src/components/auth/AuthGuard.tsx
@@ -1,4 +1,5 @@
 import { api, APIResponse } from "@/lib/api";
+import { UserType } from "@/types/user";
 import { useQuery } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 import { useNavigate } from "react-router";
@@ -6,7 +7,8 @@ import { useNavigate } from "react-router";
 export default function AuthGuard({ children }: PropsWithChildren) {
     const { isPending, isError } = useQuery({
         queryKey: ["me"],
-        queryFn: () => api.get<APIResponse>("/me").then((res) => res.data.data),
+        queryFn: () =>
+            api.get<APIResponse<UserType>>("/me").then((res) => res.data.data),
     });
 
     const navigate = useNavigate();
--- frontend/src/components/auth/GuestGuard.tsx
+++ frontend/src/components/auth/GuestGuard.tsx
@@ -1,4 +1,5 @@
 import { api, APIResponse } from "@/lib/api";
+import { UserType } from "@/types/user";
 import { useQuery } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 import { Navigate } from "react-router";
@@ -6,7 +7,8 @@ import { Navigate } from "react-router";
 export default function GuestGuard({ children }: PropsWithChildren) {
     const { isPending, isError } = useQuery({
         queryKey: ["me"],
-        queryFn: () => api.get<APIResponse>("/me").then((res) => res.data.data),
+        queryFn: () =>
+            api.get<APIResponse<UserType>>("/me").then((res) => res.data.data),
     });
 
     if (isPending) return null;

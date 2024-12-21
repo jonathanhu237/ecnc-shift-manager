0265e3c022eeb229744d4f084c68ac28c30f8e84
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-21 20:08:13 +0800

    refactor: :recycle: simplify environment variable handling by removing default values and improving logging for empty variables

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -36,61 +36,49 @@ func ReadConfig(logger *slog.Logger) (*Config, error) {
 		logger: logger,
 	}
 
-	cfg.Environment = cfg.readStringEnv("ENVIRONMENT", "development")
-	cfg.ServerPort = cfg.readIntEnv("SERVER_PORT", 8080)
-	cfg.JWTSecret = cfg.readStringEnv("JWT_SECRET", "")
+	cfg.Environment = cfg.readStringEnv("ENVIRONMENT")
+	cfg.ServerPort = cfg.readIntEnv("SERVER_PORT")
+	cfg.JWTSecret = cfg.readStringEnv("JWT_SECRET")
 
 	// postgres
-	cfg.Postgres.User = cfg.readStringEnv("POSTGRES_USER", "postgres")
-	cfg.Postgres.Password = cfg.readStringEnv("POSTGRES_PASSWORD", "")
-	cfg.Postgres.DB = cfg.readStringEnv("POSTGRES_DB", "ecnc_shift_manager_db")
+	cfg.Postgres.User = cfg.readStringEnv("POSTGRES_USER")
+	cfg.Postgres.Password = cfg.readStringEnv("POSTGRES_PASSWORD")
+	cfg.Postgres.DB = cfg.readStringEnv("POSTGRES_DB")
 
 	// rabbitmq
-	cfg.RabbitMQ.User = cfg.readStringEnv("RABBITMQ_DEFAULT_USER", "rabbitmq")
-	cfg.RabbitMQ.Password = cfg.readStringEnv("RABBITMQ_DEFAULT_PASS", "")
+	cfg.RabbitMQ.User = cfg.readStringEnv("RABBITMQ_DEFAULT_USER")
+	cfg.RabbitMQ.Password = cfg.readStringEnv("RABBITMQ_DEFAULT_PASS")
 
 	// Email
-	cfg.MailClient.SMTPHost = cfg.readStringEnv("MAIL_CLIENT_SMTP_HOST", "")
-	cfg.MailClient.Sender = cfg.readStringEnv("MAIL_CLIENT_SENDER", "")
-	cfg.MailClient.Password = cfg.readStringEnv("MAIL_CLIENT_PASSWORD", "")
+	cfg.MailClient.SMTPHost = cfg.readStringEnv("MAIL_CLIENT_SMTP_HOST")
+	cfg.MailClient.Sender = cfg.readStringEnv("MAIL_CLIENT_SENDER")
+	cfg.MailClient.Password = cfg.readStringEnv("MAIL_CLIENT_PASSWORD")
 
 	return cfg, nil
 }
 
-func (cfg *Config) readStringEnv(key, defaultValue string) string {
-	val, ok := os.LookupEnv(key)
-	if !ok {
-		cfg.logger.Warn(
-			"Environment variable not found, use default value instead.",
-			slog.String("key", key),
-			slog.String("default_value", defaultValue),
-		)
-		return defaultValue
+func (cfg *Config) readStringEnv(key string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		cfg.logger.Warn("Environment variable is empty.", slog.String("key", key))
 	}
-
 	return val
 }
 
-func (cfg *Config) readIntEnv(key string, defaultValue int) int {
-	val, ok := os.LookupEnv(key)
-	if !ok {
-		cfg.logger.Warn(
-			"Environment variable not found, use default value instead.",
-			slog.String("key", key),
-			slog.Int("default_value", defaultValue),
-		)
-		return defaultValue
+func (cfg *Config) readIntEnv(key string) int {
+	val := os.Getenv(key)
+	if val == "" {
+		cfg.logger.Warn("Environment variable is empty.", slog.String("key", key))
 	}
 
 	intVal, err := strconv.Atoi(val)
 	if err != nil {
 		cfg.logger.Warn(
-			"Environment variable is not a valid integer, use default value instead.",
+			"Environment variable is not a valid integer, use zero instead.",
 			slog.String("key", key),
 			slog.String("value", val),
-			slog.Int("default_value", defaultValue),
 		)
-		return defaultValue
+		return 0
 	}
 
 	return intVal

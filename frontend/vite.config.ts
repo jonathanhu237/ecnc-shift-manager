9996044b5c1ac66e0a42820a71702ecb3b55d2b0
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-25 22:04:57 +0800

    fix: :bug: update Vite config to load environment variables for backend URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  define: {
-    "process.env": process.env,
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target:
-          (process.env.API_SERVER_HOST ?? "http://localhost") +
-          ":" +
-          (process.env.API_SERVER_PORT ?? "8080"),
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });

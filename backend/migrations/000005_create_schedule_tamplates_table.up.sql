9dbfae0f5777c990087c92a3a8b7306f77944927
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-27 22:59:25 +0800

    fix: :bug: change start_time and end_time types to TIME WITH TIME ZONE for accuracy

--- backend/migrations/000005_create_schedule_tamplates_table.up.sql
+++ backend/migrations/000005_create_schedule_tamplates_table.up.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS schedule_template_shifts (
     id BIGSERIAL PRIMARY KEY,
     schedule_template_id BIGINT REFERENCES schedule_templates(id) ON DELETE CASCADE,
     day_of_week INTEGER NOT NULL,
-    start_time TIME NOT NULL,
-    end_time TIME NOT NULL,
+    start_time TIME WITH TIME ZONE NOT NULL,
+    end_time TIME WITH TIME ZONE NOT NULL,
     assistants_required INTEGER NOT NULL
 );

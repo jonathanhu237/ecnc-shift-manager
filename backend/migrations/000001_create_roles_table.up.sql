84c0df5141b84ddec0ed5f793bf0d7fa42f180a3
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-28 19:07:06 +0800

    fix: :bug: add missing semicolon in roles and users table migration scripts

--- backend/migrations/000001_create_roles_table.up.sql
+++ backend/migrations/000001_create_roles_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS roles (
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
     level INTEGER NOT NULL
-)
+);
 
 INSERT INTO roles (id, name, level) VALUES 
     (1, '普通助理', 1),
--- backend/migrations/000002_create_users_table.up.sql
+++ backend/migrations/000002_create_users_table.up.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS users (
     password_hash TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     full_name TEXT NOT NULL,
-    role_id BIGINT NOT NULL REFERENCES roles(id)
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    role_id BIGINT NOT NULL REFERENCES roles(id),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );

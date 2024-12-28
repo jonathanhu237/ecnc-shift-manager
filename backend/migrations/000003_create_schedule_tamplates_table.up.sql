6512dff6a065cf9530cfd7a00bb17af86a9eeeb4
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-28 20:07:20 +0800

    refactor: :recycle: update schedule_templates table to use TIMESTAMPTZ and TIMETZ for time-related fields

--- backend/migrations/000003_create_schedule_tamplates_table.up.sql
+++ backend/migrations/000003_create_schedule_tamplates_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS schedule_templates (
     id BIGSERIAL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
     description TEXT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     version INTEGER NOT NULL DEFAULT 1
 );
 
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS schedule_template_shifts (
     id BIGSERIAL PRIMARY KEY,
     schedule_template_id BIGINT REFERENCES schedule_templates(id) ON DELETE CASCADE,
     day_of_week INTEGER NOT NULL,
-    start_time TIME WITH TIME ZONE NOT NULL,
-    end_time TIME WITH TIME ZONE NOT NULL,
+    start_time TIMETZ NOT NULL,
+    end_time TIMETZ NOT NULL,
     assistants_required INTEGER NOT NULL
 );

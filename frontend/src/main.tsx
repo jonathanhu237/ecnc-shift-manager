094bebd55b588db0ca1a5b6be547ed2395ae1561
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-29 10:42:32 +0800

    feat: :sparkles: create ScheduleTemplateMetaTable component and update ShiftTemplatesManagement to use it

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,51 +11,48 @@ import DashboardIndex from "./pages/DashboardIndex.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import UsersManagementPage from "./pages/UsersManagementPage.tsx";
 import BlackCoreGuard from "./components/auth/BlackCoreGuard.tsx";
-import ShiftTemplatesManagement from "./pages/ShiftTemplatesManagement.tsx";
+import ScheduleTemplatesManagement from "./pages/ShiftTemplatesManagement.tsx";
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: Infinity,
-            retry: false,
-        },
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      retry: false,
     },
+  },
 });
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<DashboardLayout />}>
-                        <Route index element={<DashboardIndex />} />
-                        <Route
-                            path="users-management"
-                            element={
-                                <BlackCoreGuard>
-                                    <UsersManagementPage />
-                                </BlackCoreGuard>
-                            }
-                        />
-                        <Route
-                            path="shift-templates-management"
-                            element={
-                                <BlackCoreGuard>
-                                    <ShiftTemplatesManagement />
-                                </BlackCoreGuard>
-                            }
-                        />
-                    </Route>
-                    <Route path="auth" element={<AuthLayout />}>
-                        <Route path="login" element={<LoginPage />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-            <ReactQueryDevtools
-                initialIsOpen={false}
-                buttonPosition="top-right"
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DashboardLayout />}>
+            <Route index element={<DashboardIndex />} />
+            <Route
+              path="users-management"
+              element={
+                <BlackCoreGuard>
+                  <UsersManagementPage />
+                </BlackCoreGuard>
+              }
             />
-            <Toaster />
-        </QueryClientProvider>
-    </StrictMode>
+            <Route
+              path="shift-templates-management"
+              element={
+                <BlackCoreGuard>
+                  <ScheduleTemplatesManagement />
+                </BlackCoreGuard>
+              }
+            />
+          </Route>
+          <Route path="auth" element={<AuthLayout />}>
+            <Route path="login" element={<LoginPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-right" />
+      <Toaster />
+    </QueryClientProvider>
+  </StrictMode>
 );
--- frontend/src/pages/ShiftTemplatesManagement.tsx
+++ frontend/src/pages/ShiftTemplatesManagement.tsx
@@ -1,29 +1,32 @@
+import ScheduleTemplateMetaTable from "@/components/ScheduleTemplateMetaTable";
 import {
-    Breadcrumb,
-    BreadcrumbList,
-    BreadcrumbItem,
-    BreadcrumbPage,
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbPage,
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 
-export default function ShiftTemplatesManagement() {
-    return (
-        <SidebarInset className="flex flex-col h-full">
-            <header className="flex h-12 items-center gap-2">
-                <SidebarTrigger className="ml-2" />
-                <Separator orientation="vertical" className="h-4" />
-                <Breadcrumb>
-                    <BreadcrumbList>
-                        <BreadcrumbItem>
-                            <BreadcrumbPage>班表模板管理</BreadcrumbPage>
-                        </BreadcrumbItem>
-                    </BreadcrumbList>
-                </Breadcrumb>
-            </header>
-            <main className="px-2">
-                <div>ShiftTemplatesManagement</div>
-            </main>
-        </SidebarInset>
-    );
+export default function ScheduleTemplatesManagement() {
+  return (
+    <SidebarInset className="flex flex-col h-full">
+      <header className="flex h-12 items-center gap-2">
+        <SidebarTrigger className="ml-2" />
+        <Separator orientation="vertical" className="h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbPage>班表模板管理</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </header>
+      <main className="px-2">
+        <div className="container mx-auto">
+          <ScheduleTemplateMetaTable />
+        </div>
+      </main>
+    </SidebarInset>
+  );
 }

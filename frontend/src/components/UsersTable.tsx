f084872628091a754b02b93255c1370bfcb31e73
jonathanhu237 <jonathanhu237@gmail.com>
2024-12-29 10:40:49 +0800

    style: :art: format UsersTable component for improved readability

--- frontend/src/components/UsersTable.tsx
+++ frontend/src/components/UsersTable.tsx
@@ -5,12 +5,12 @@ import { ColumnDef } from "@tanstack/react-table";
 import DataTable from "./DataTable";
 import { toast } from "sonner";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { ArrowUpDown, MoreHorizontal } from "lucide-react";
@@ -20,135 +20,129 @@ import DeleteUserDialog from "./DeleteUserDialog";
 import CreateUserDialog from "./CreateUserDialog";
 
 export default function UsersTable() {
-    const { data, isPending, isError, error } = useQuery({
-        queryKey: ["users"],
-        queryFn: () =>
-            api
-                .get<APIResponse<UserType[]>>("/users")
-                .then((res) => res.data.data),
-    });
-    const [updateRoleDialogOpen, setUpdateRoleDialogOpen] = useState(false);
-    const [deleteUserDialogOpen, setDeleteUserDialogOpen] = useState(false);
-    const [currentUser, setCurrentUser] = useState<UserType | null>(null);
-    const [createUserDialogOpen, setCreateUserDialogOpen] = useState(false);
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["users"],
+    queryFn: () =>
+      api.get<APIResponse<UserType[]>>("/users").then((res) => res.data.data),
+  });
+  const [updateRoleDialogOpen, setUpdateRoleDialogOpen] = useState(false);
+  const [deleteUserDialogOpen, setDeleteUserDialogOpen] = useState(false);
+  const [currentUser, setCurrentUser] = useState<UserType | null>(null);
+  const [createUserDialogOpen, setCreateUserDialogOpen] = useState(false);
 
-    const columns: ColumnDef<UserType>[] = [
-        {
-            accessorKey: "username",
-            header: ({ column }) => {
-                return (
-                    <Button
-                        variant="ghost"
-                        onClick={() => {
-                            column.toggleSorting(
-                                column.getIsSorted() === "asc"
-                            );
-                        }}
-                    >
-                        用户名
-                        <ArrowUpDown className="ml-2 h-4 w-4" />
-                    </Button>
-                );
-            },
-        },
-        {
-            accessorKey: "fullName",
-            header: "姓名",
-        },
-        {
-            accessorKey: "email",
-            header: "邮箱",
-        },
-        {
-            accessorKey: "role",
-            header: ({ column }) => {
-                return (
-                    <Button
-                        variant="ghost"
-                        onClick={() => {
-                            column.toggleSorting(
-                                column.getIsSorted() === "asc"
-                            );
-                        }}
-                    >
-                        身份
-                        <ArrowUpDown className="ml-2 h-4 w-4" />
-                    </Button>
-                );
-            },
-        },
-        {
-            id: "action",
-            cell: ({ row }) => {
-                const user = row.original;
+  const columns: ColumnDef<UserType>[] = [
+    {
+      accessorKey: "username",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => {
+              column.toggleSorting(column.getIsSorted() === "asc");
+            }}
+          >
+            用户名
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+    },
+    {
+      accessorKey: "fullName",
+      header: "姓名",
+    },
+    {
+      accessorKey: "email",
+      header: "邮箱",
+    },
+    {
+      accessorKey: "role",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => {
+              column.toggleSorting(column.getIsSorted() === "asc");
+            }}
+          >
+            身份
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+    },
+    {
+      id: "action",
+      cell: ({ row }) => {
+        const user = row.original;
 
-                return (
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="ghost" className="h-8 w-8 p-0">
-                                <MoreHorizontal className="h-4 w-4" />
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuLabel>操作</DropdownMenuLabel>
-                            <DropdownMenuItem
-                                onClick={() => {
-                                    setUpdateRoleDialogOpen(true);
-                                    setCurrentUser(user);
-                                }}
-                            >
-                                更改身份
-                            </DropdownMenuItem>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem
-                                className="text-destructive"
-                                onClick={() => {
-                                    setDeleteUserDialogOpen(true);
-                                    setCurrentUser(user);
-                                }}
-                            >
-                                删除用户
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                );
-            },
-        },
-    ];
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>操作</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => {
+                  setUpdateRoleDialogOpen(true);
+                  setCurrentUser(user);
+                }}
+              >
+                更改身份
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="text-destructive"
+                onClick={() => {
+                  setDeleteUserDialogOpen(true);
+                  setCurrentUser(user);
+                }}
+              >
+                删除用户
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
 
-    if (isPending) {
-        return null;
-    }
-    if (isError) {
-        toast.error(error.message);
-        return null;
-    }
+  if (isPending) {
+    return null;
+  }
+  if (isError) {
+    toast.error(error.message);
+    return null;
+  }
 
-    return (
-        <>
-            <div className="flex items-center justify-end">
-                <Button
-                    className="mb-4 mt-2"
-                    onClick={() => setCreateUserDialogOpen(true)}
-                >
-                    添加用户
-                </Button>
-            </div>
-            <DataTable columns={columns} data={data} />
-            <UpdateUserRoleDialog
-                user={currentUser}
-                open={updateRoleDialogOpen}
-                onOpenChange={setUpdateRoleDialogOpen}
-            />
-            <DeleteUserDialog
-                user={currentUser}
-                open={deleteUserDialogOpen}
-                onOpenChange={setDeleteUserDialogOpen}
-            />
-            <CreateUserDialog
-                open={createUserDialogOpen}
-                onOpenChange={setCreateUserDialogOpen}
-            />
-        </>
-    );
+  return (
+    <>
+      <div className="flex items-center justify-end">
+        <Button
+          className="mb-4 mt-2"
+          onClick={() => setCreateUserDialogOpen(true)}
+        >
+          添加用户
+        </Button>
+      </div>
+      <DataTable columns={columns} data={data} />
+      <UpdateUserRoleDialog
+        user={currentUser}
+        open={updateRoleDialogOpen}
+        onOpenChange={setUpdateRoleDialogOpen}
+      />
+      <DeleteUserDialog
+        user={currentUser}
+        open={deleteUserDialogOpen}
+        onOpenChange={setDeleteUserDialogOpen}
+      />
+      <CreateUserDialog
+        open={createUserDialogOpen}
+        onOpenChange={setCreateUserDialogOpen}
+      />
+    </>
+  );
 }
